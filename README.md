d3fb81c548cf887991470bd8c9e6ace1ce0d0756
pld <peter@helioid.com>
2013-09-01 16:42:09 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fdg
+
+A Clojure library designed to compute functional differential geometry.
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2013 Peter Lubell-Doughtie
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject fdg "0.1.0-SNAPSHOT"
+  :description "Functional Differential Programming"
+  :url "http://github.com/pld/fdg"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
