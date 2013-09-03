fdd230001328156f9c2f54fe3f1f746ac0774abe
pld <peter@helioid.com>
2013-09-03 10:21:44 -0400

    add midje

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/pld/fdg"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  :plugins [[lein-midje "3.0.0"]])
